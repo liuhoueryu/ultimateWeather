1e7f6ae57f4e9f51387b86b6fa9ae91be8fb216e
liuhoueryu <yizhziy@foxmail.com>
2019-03-30 12:08:48 +0800

    兼容数据库

--- src/com/neuedu/util/DBManager.java
+++ src/com/neuedu/util/DBManager.java
@@ -11,41 +11,42 @@ import java.util.Properties;
 public class DBManager {
 
 	private static Properties prop = new Properties();
-	
+
 	private static String dbDriver;
 	private static String dbUrl;
 	private static String dbUsername;
 	private static String dbPassword;
-	
-	//初始化
-	static{
-		
+
+	// 初始化
+	static {
+
 		try {
-			//加载属性文件到内存
+			// 加载属性文件到内存
 			prop.load(DBManager.class.getResourceAsStream("db-config.properties"));
-			
-			//读取数据库的连接信息
+
+			// 读取数据库的连接信息
 			dbDriver = prop.getProperty("dbDriver");
 			dbUrl = prop.getProperty("dbUrl");
 			dbUsername = prop.getProperty("dbUsername");
 			dbPassword = prop.getProperty("dbPassword");
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private Connection conn;
 	private PreparedStatement pstmt;
 
-	//单例
+	// 单例
 	private static DBManager instance = new DBManager();
-	public static DBManager getInstance(){
+
+	public static DBManager getInstance() {
 		return instance;
 	}
-	
-    //构造方法---加载数据库驱动
-	private DBManager(){
+
+	// 构造方法---加载数据库驱动
+	private DBManager() {
 		try {
 			Class.forName(dbDriver);
 		} catch (ClassNotFoundException e) {
@@ -53,73 +54,73 @@ public class DBManager {
 		}
 	}
 
-	//打开数据库连接
-	private void openConnection(){
+	// 打开数据库连接
+	private void openConnection() {
 		try {
-			this.conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tylgky", dbUsername, dbPassword);
+			this.conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/haoren", dbUsername, dbPassword);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 
-	//更新方法
-	public boolean execUpdate(String sql, Object... params){
-		
+	// 更新方法
+	public boolean execUpdate(String sql, Object... params) {
+
 		this.openConnection();
-		
+
 		try {
 			pstmt = this.conn.prepareStatement(sql);
-			
-			//参数赋值
+
+			// 参数赋值
 			for (int i = 0; i < params.length; i++) {
-				pstmt.setObject(i+1, params[i]);
+				pstmt.setObject(i + 1, params[i]);
 			}
-			
+
 			return pstmt.executeUpdate() > 0;
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return false;
-		} finally{
+		} finally {
 			this.closeConnection();
 		}
-		
+
 	}
-	
-	//查询方法
-	public ResultSet execQuery(String sql, Object... params){	
-		
+
+	// 查询方法
+	public ResultSet execQuery(String sql, Object... params) {
+
 		this.openConnection();
-		
+
 		try {
 			pstmt = conn.prepareStatement(sql);
-			
-			//参数赋值
+
+			// 参数赋值
 			for (int i = 0; i < params.length; i++) {
-				pstmt.setObject(i+1, params[i]);
+				pstmt.setObject(i + 1, params[i]);
 			}
-			
+
 			return pstmt.executeQuery();
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return null;
 		}
-		
+
 	}
-	
-	//关闭数据库连接
-	public void closeConnection(){
-			
-		if(this.pstmt!=null){
+
+	// 关闭数据库连接
+	public void closeConnection() {
+
+		if (this.pstmt != null) {
 			try {
 				this.pstmt.close();
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
-		
-		if(this.conn!=null){
+
+		if (this.conn != null) {
 			try {
 				this.conn.close();
 			} catch (SQLException e) {
@@ -127,43 +128,27 @@ public class DBManager {
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		DBManager dbManager = DBManager.getInstance();
-		
-		/*
-		String sql = "update user set score = score - ?";
-		dbManager.execUpdate(sql, 2);
-		*/
-		
-		String sql = "select * from news where newsid > ?";
-		ResultSet rs = dbManager.execQuery(sql, 20);
-		
+
+		String sql = "select * from weather where city like ? and province like ? order by province asc, city asc,date desc";
+
+		String city="晋城";
+		String province="山西";
+		ResultSet rs = dbManager.execQuery(sql,city,province);
+
 		try {
-			while(rs.next()){
-				System.out.println(rs.getInt(1) + "-" + rs.getString(2) + "-" + rs.getString(3) + "-" + rs.getInt(4));
+			while (rs.next()) {
+				System.out.println(rs.getString(1) + "-" + rs.getString(2) + "-" + rs.getString(4) + "-" + rs.getInt(5));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} finally{
+		} finally {
 			dbManager.closeConnection();
 		}
-		
-		/*String sql = "delete from news";
 
-		dbManager.execUpdate(sql);
-		
-		sql = "insert into news values(null, ?, ?, 1, 1, now())";
+	}
 
-		for(int i=1; i<100; i++){
-		
-			String title = "新闻主题" + i;
-
-			String content = "新闻内容" + i;
-			
-			dbManager.execUpdate(sql, title, content);*/
-		}
-		
-	
 }
